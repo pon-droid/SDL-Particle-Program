eb41739235899c322acdd5b6551e7c642adb815e
pon-droid <tellepsarecool@gmail.com>
2021-09-01 15:44:32 +1000

    Added changing trig values based on ticks elapsed, values are then used for pixel set method

--- src/SDL2 Pixel iStem.cpp
+++ src/SDL2 Pixel iStem.cpp
@@ -11,14 +11,21 @@ int main(int argcount, char **args) {
 	Screen screen;
 
 	while (true) {
+		int ticks = SDL_GetTicks();
+		//Sine locks value to -1 +1 and gradually bounces up and down
+		//unsigned is there to ensure we do not receive a negative integer
+
+		unsigned char red = (1+(sin(ticks*0.0002)))*128;
+		unsigned char green = (1+(sin(ticks*0.0001)))*128;
+		unsigned char blue = (1+(sin(ticks*0.0003)))*128;
 
 		for(int y=0;y<Screen::SCREEN_HEIGHT;y++){
 			for(int x=0;x<Screen::SCREEN_WIDTH;x++){
-				screen.Set_Pixel(x,y,128,128,0,255);
+				screen.Set_Pixel(x,y,red,green,blue,255);
 			}
 		}
 
-		screen.Set_Pixel(400, 300, 255, 255, 255, 255);
+
 
 		screen.Update();
 
